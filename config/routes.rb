e8417830c4a038533d06e2b061c28b7c69dffc3c
Dmitriy Strukov <dmitiry_strukov2011@mail.ru>
2018-03-09 11:31:15 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,25 @@ Spree::Core::Engine.routes.draw do
 
   namespace :admin do
     resources :currency_rates
+
     resources :price_books do
       resources :products, only: [] do
         get :remove_product, controller:'price_books'
         get :edit_product_price, controller:'price_books'
       end
+
       member do
         get :add_products
         patch :products
       end
     end
+
     resources :products do
       member do
         get :variant_prices
       end
     end
+
     resources :stores do
       resources :price_books do
         collection do
@@ -24,7 +28,7 @@ Spree::Core::Engine.routes.draw do
         end
       end
     end
+
     put 'update_variants_prices', to: 'prices#update'
   end
-
 end
